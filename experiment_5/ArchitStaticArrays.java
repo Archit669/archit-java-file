b2587da6c5cceaf161a7356ca605fd0153fcc748
archit669 <architjain669@gmail.com>
2023-03-27 16:09:28 +0530

    add exp4 and exp5

---
+++ experiment_5/ArchitStaticArrays.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class ArchitStaticArrays{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // take size of array from user
+        System.out.print("enter size of array: ");
+        int n = sc.nextInt();
+
+        // create static array
+        int []arr = {1,2,3,4,5};
+
+        // print array
+        System.out.println("array is : ");
+        for (int x : arr){
+            System.out.print(x + " ");
+        }System.out.println();
+
+    }
+}
